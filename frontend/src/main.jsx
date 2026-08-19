26a6f7233dc11ebb2256cc53ae003efd6a1e27c3
Bruce Hung <brucewmhung@gmail.com>
2026-08-19 21:04:28 +0800

    Optimize page load

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './i18n.js';
+import './styles/fonts.css';
 import './styles/theme.css';
 
 createRoot(document.getElementById('root')).render(
